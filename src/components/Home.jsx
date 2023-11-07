d3514f4e1bce0014bd8ec2570bb859a95498ed91
Siddhi Dingorkar <siddhidingorkar@gmail.com>
2023-11-07 15:08:07 +0530

    second commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css"; // Import your CSS file here
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h1 className="centered-heading">Welcome to Open Minds!</h1>
+      <h1> "Every Child Deserves a Chance to Learn"</h1>
+    </div>
+  );
+}
+
+export default HomePage;
