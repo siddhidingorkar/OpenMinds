d52bdfac8eb94c094806957e616b9313f22248d9
Siddhi Dingorkar <siddhidingorkar@gmail.com>
2023-11-07 14:54:37 +0530

    first commit

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const signup = () => {
   return (
     <div>
-      
+      <h1> Welcome to Open Minds!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default signup
-
+export default signup;
