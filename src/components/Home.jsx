cbf3fefbb8444a59cdef24c6a13fea56d0ebf41f
Shekhar <shekkharkalshetti@gmail.com>
2023-11-07 15:13:22 +0530

    testing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import "./Home.css"; // Import your CSS file here
 function HomePage() {
   return (
     <div className="home-page">
-      <h1 className="centered-heading">Welcome to Open Minds!</h1>
+      <h1 className="centered-heading">Shiddu Dhungu</h1>
       <h1> "Every Child Deserves a Chance to Learn"</h1>
     </div>
   );
