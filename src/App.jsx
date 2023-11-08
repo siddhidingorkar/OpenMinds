dc1ea41d4ba8fc38e69d46906c0040fa936bed91
Shekhar <shekkharkalshetti@gmail.com>
2023-11-08 10:18:38 +0530

    added about  us

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/navbar";
+import AboutUs from "./pages/AboutUs";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      <AboutUs />
     </div>
   );
 }
